0f7d78201fb2640733dce4495052bd35407c1120
Ilia Komarov <nexen505@yandex.ru>
2020-04-30 13:26:12 +0400

    Exercise 11.

--- exercises/exercise-11/declarations/stats/index.d.ts
+++ exercises/exercise-11/declarations/stats/index.d.ts
@@ -1,3 +1,12 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    export type Comparator<T> = (a: T, b: T) => number;
+    export type ValueExtractor<T> = (a: T) => number;
+
+    export function getMaxIndex<T>(input: ReadonlyArray<T>, comparator: Comparator<T>): number;
+    export function getMaxElement<T>(input: ReadonlyArray<T>, comparator: Comparator<T>): T | null;
+    export function getMinIndex<T>(input: ReadonlyArray<T>, comparator: Comparator<T>): number;
+    export function getMinElement<T>(input: ReadonlyArray<T>, comparator: Comparator<T>): T | null;
+    export function getMedianIndex<T>(input: ReadonlyArray<T>, comparator: Comparator<T>): number;
+    export function getMedianElement<T>(input: ReadonlyArray<T>, comparator: Comparator<T>): T | null;
+    export function getAverageValue<T>(input: ReadonlyArray<T>, extractor: ValueExtractor<T>): number;
 }
