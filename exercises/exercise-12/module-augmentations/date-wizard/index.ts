f9d6356e217dae503e019cbcc394c97428f5b3f7
Ilia Komarov <nexen505@yandex.ru>
2020-04-30 13:38:00 +0400

    Exercise 12.

--- exercises/exercise-12/module-augmentations/date-wizard/index.ts
+++ exercises/exercise-12/module-augmentations/date-wizard/index.ts
@@ -2,5 +2,14 @@
 import 'date-wizard';
 
 declare module 'date-wizard' {
-    // Add your module extensions here.
+    export interface DateDetails {
+        year: number;
+        month: number;
+        date: number;
+        hours: number;
+        minutes: number;
+        seconds: number;
+    }
+
+    export function pad(value: any): string;
 }
